7c76967804018c1547246ac6393f749dd02fdbb5
Anton Trifonov <toha62@inbox.ru>
2022-11-27 11:58:11 +0300

    version 1.5

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,16 +1,64 @@
 class CookieClicker {
-  constructor(imgId, clickerCounterId, clickerSpeedId) {
+  constructor(imgId, clickerCounterId, clickerSpeedId, buttonNewId, buttonEndId) {
     this.imgElement = document.getElementById(imgId);
     this.clickerCounterElement = document.getElementById(clickerCounterId);
     this.clickerSpeedElement = document.getElementById(clickerSpeedId);
+    this.buttonNewElement = document.getElementById(buttonNewId);
+    this.buttonEndElement = document.getElementById(buttonEndId);
+    this.cardElement = document.querySelector('.card');
+    this.resetGameData();
+  }
+
+  resetGameData() {
+    this.imgElement.onclick = null;
+    this.buttonNewElement.onclick = this.startGame.bind(this);
+    this.buttonEndElement.onclick = null;
+    this.clickerCounterElement.innerText = 0;
+    this.clickerSpeedElement.innerText = 0;    
+  }
+
+  startGame() {
+    this.cardElement.style.display = 'block';
     this.imgElement.onclick = this.onClickReaction.bind(this);
+    this.buttonNewElement.onclick = this.restartGame.bind(this);
+    this.buttonEndElement.onclick = this.endGame.bind(this);
     this.previousClickTime = null;
+    this.maxClickSpeed = 0;
+  }  
+
+  restartGame() {
+    if (confirm('Вы уверены, что хотите начать новую игру ?')) {
+      this.resetGameData();
+      this.startGame();
+    }
+  }
+
+  endGame() {
+    if (confirm('Вы уверены, что хотите завершить игру ?')) {      
+      alert(`Вы сделали: ${this.clickerCounterElement.innerText} кликов. 
+            Ваша максимальная скорость: ${this.maxClickSpeed}`);
+      this.cardElement.style.display = 'none';
+      this.resetGameData();
+    }    
   }
 
   onClickReaction() {    
-    this.calculateClickerSpeed();
+    this.calculateClickerSpeedAndMax();
     this.changeSizeImg();
     this.increaseClickerCounter();
+  }
+
+  calculateClickerSpeedAndMax() {
+    let clickSpeed;
+    this.curentClickTime = new Date;
+    if (this.previousClickTime) {
+      clickSpeed = (1000 / (this.curentClickTime - this.previousClickTime)).toFixed(2);
+      this.clickerSpeedElement.innerText = clickSpeed;
+      if (clickSpeed > this.maxClickSpeed) {
+        this.maxClickSpeed = clickSpeed;
+      }
+    }    
+    this.previousClickTime = this.curentClickTime;
   }
 
   changeSizeImg() {
@@ -21,14 +69,7 @@ class CookieClicker {
     this.clickerCounterElement.innerText = +this.clickerCounterElement.innerText + 1;
   }
 
-  calculateClickerSpeed() {
-    this.curentClickTime = new Date;
-    if (this.previousClickTime) {
-      this.clickerSpeedElement.innerText = (1000 / (this.curentClickTime - this.previousClickTime)).toFixed(2);      
-    }    
-    this.previousClickTime = this.curentClickTime;
-  }
-
+  
 }
 
-const cookieClicker = new CookieClicker('cookie', 'clicker__counter', 'clicker__speed');
+const cookieClicker = new CookieClicker('cookie', 'clicker__counter', 'clicker__speed', 'button-new', 'button-end');
